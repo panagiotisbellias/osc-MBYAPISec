19b863772b3017ca86e997f9cdcec381a6d5a8c5
Marcus Lull <marcuslull@gmail.com>
2024-02-18 08:40:30 -0800

    intitial commit

---
+++ src/test/java/com/marcuslull/mbyapisec/MbyapiSecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcuslull.mbyapisec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MbyapiSecApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
